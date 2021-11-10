040fce307b422f27c741757d20476093cd131d4b
rajan <rajan.metaliya@nonstopio.com>
2021-11-10 16:04:08 +0530

    Added proxy URL to linkedinAuth

--- lib/linkedin_oauth2_client.dart
+++ lib/linkedin_oauth2_client.dart
@@ -5,11 +5,15 @@ import 'package:oauth2_client/oauth2_client.dart';
 /// In order to use this client you need to first configure OAuth2 credentials (see https://docs.microsoft.com/it-it/linkedin/shared/authentication/authorization-code-flow)
 ///
 class LinkedInOAuth2Client extends OAuth2Client {
-  LinkedInOAuth2Client(
-      {required String redirectUri, required String customUriScheme})
-      : super(
-            authorizeUrl: 'https://www.linkedin.com/oauth/v2/authorization',
-            tokenUrl: 'https://www.linkedin.com/oauth/v2/accessToken',
+  LinkedInOAuth2Client({
+    required String redirectUri,
+    required String customUriScheme,
+    required String proxyURL,
+  }) : super(
+            authorizeUrl:
+                proxyURL + 'https://www.linkedin.com/oauth/v2/authorization',
+            tokenUrl:
+                proxyURL + 'https://www.linkedin.com/oauth/v2/accessToken',
             redirectUri: redirectUri,
             customUriScheme: customUriScheme);
 }
